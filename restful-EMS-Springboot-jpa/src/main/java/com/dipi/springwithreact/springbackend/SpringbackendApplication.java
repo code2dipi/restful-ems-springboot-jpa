e9bcd0a283795483a2b0621790f31f305a74df7e
code2dipi <63505385+code2dipi@users.noreply.github.com>
2020-10-02 23:58:49 +0200

    1 commit

--- restful-EMS-Springboot-jpa/src/main/java/com/dipi/springwithreact/springbackend/SpringbackendApplication.java
+++ restful-EMS-Springboot-jpa/src/main/java/com/dipi/springwithreact/springbackend/SpringbackendApplication.java
@@ -3,6 +3,7 @@ package com.dipi.springwithreact.springbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringbackendApplication {
 
